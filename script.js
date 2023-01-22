f26e520fe700fc1657e4f67093b25806fee5c0ca
Jama <jamshid.abdumusayev@mail.ru>
2023-01-22 17:37:37 +0500

    JS homework-1

---
+++ script.js
@@ -0,0 +1,29 @@
+// var name = prompt('Ваше имя');
+//     console.log(name);
+    
+// var age = prompt('Ваш возраст');
+//     console.log(age);    
+    
+// var example1 = +prompt(' 6 + 3 = ');
+//     console.log(' 6 + 3 = ' + (6 + 3) + ' Ваш ответ ' +example1);    
+
+// var example1 = +prompt(' 6 - 3 = ');
+//     console.log(' 6 - 3 = ' + (6 - 3) + ' Ваш ответ ' +example1);
+
+// var example1 = +prompt(' 6 * 3 = '); 
+//     console.log(' 6 * 3 = ' + (6 * 3) + ' Ваш ответ ' +example1); 
+    
+// var example1 = +prompt(' 10 / 3 = '); 
+//     console.log(' 10 / 3 = ' + (10 & 3) + ' Ваш ответ ' +example1);
+    
+
+    
+var a = +prompt('Любое число');    
+var b = +prompt('Любое число');    
+var c = +prompt('Любое число');    
+    console.log(' (a + b + c) / 3 = ' + (a + b + c) / 3);
+    
+var res = ((a + b + c) / 3);
+    alert((a + b + c) / 3)
+   
+
